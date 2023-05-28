f842cb5b3e83814ba8a1b290fe578a06f5be6734
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2023-05-28 14:10:18 +0000

    non va

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask import jsonify
 from flask import request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+import geojson
+import shapely.wkt
 
 app = Flask(__name__)
 
@@ -49,7 +51,8 @@ def get_vettore(foglio):
             "CI_VETTORE":s['CI_VETTORE']
         })
 
-    return jsonify(output)
+    return jsonify(output) #Nota che abbiamo eliminato la chiave result perchè i dati sono già formattati
+
 
 @app.route('/ci_vettore/sezione/<sezione>', methods=['GET'])
 def get_vettoreSez(sezione):
@@ -69,9 +72,89 @@ def get_vettoreSez(sezione):
             "CI_VETTORE":s['CI_VETTORE']
         })
 
-    return jsonify(output)
- #Nota che abbiamo eliminato la chiave result perchè i dati sono già formattati
-# Checks to see if the name of the package is the run as the main package.
+    return jsonify(output) #Nota che abbiamo eliminato la chiave result perchè i dati sono già formattati
+
+@app.route('/geogeom', methods=['GET'])
+def get_all_stars():
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+
+    match = {
+        '$match': {
+            'EP_H_ND': {'$gt': 0}
+        }
+    }
+    group = {
+        '$group': {
+            '_id': {
+                'SEZ': '$SEZ',
+                'WKT': '$WKT'
+            },
+            'AVG': {
+                '$avg': '$EP_H_ND'
+            },
+            'SUM': {
+                '$sum': '$EP_H_ND'
+            }
+        }
+    }
+    limit = {
+        '$limit' : 10
+    }
+    
+    for s in mil4326WKT.aggregate([match, group, limit]):
+        g1= shapely.wkt.loads(s['_id']['WKT'])
+        g2 = geojson.Feature(geometry=g1, 
+        properties={'id':s['_id']['SEZ'], 'media':s['AVG'], 'somma':s['SUM'], 'sezione':s['_id']['SEZ']}) 
+        output.append(g2)                 
+    return jsonify({'result': output})
+
+
+# Estrae consumo energetico medio e totale raggruppato per sezione catastale (e Poligono WKT)
+@app.route('/geogeom/<float:lng>/<float:lat>/<float:r>', methods=['GET'])
+def get_avg(lng, lat, r):
+    print(type(lng))
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+
+    match = {
+        '$match': {
+            '$and':
+            [
+                {'EP_H_ND': {'$gt': 0}},
+                {'WGS84_X': {'$gt': lng - r}},
+                {'WGS84_X': {'$lt': lng + r}},
+                {'WGS84_Y': {'$gt': lat - r}},
+                {'WGS84_Y': {'$lt': lat + r}}
+            ]
+        }
+    }
+    group = {
+        '$group': {
+            '_id': {
+                'SEZ': '$SEZ',
+                'WKT': '$WKT'
+            },
+            'AVG': {
+                '$avg': '$EP_H_ND'
+            },
+            'SUM': {
+                '$sum': '$EP_H_ND'
+            }
+        }
+    }
+    limit = {
+        '$limit': 10
+    }
+
+    for s in mil4326WKT.aggregate([match, group, limit]):
+        # Converte da WKT in GeoJson Geometry
+        g1 = shapely.wkt.loads(s['_id']['WKT'])
+        g2 = geojson.Feature(geometry=g1,
+                             properties={'id': s['_id']['SEZ'], 'media': s['AVG'], 'somma': s['SUM'], 'sezione': s['_id']['SEZ']})
+        output.append(g2)
+    return jsonify(geojson.FeatureCollection(output))
+    
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
     app.run()
