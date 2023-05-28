cdc46c528b353d7f6d8ee67c1b9a126de5880041
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2023-05-28 17:34:08 +0000

    punto 4

--- ripassoPython/main.py
+++ ripassoPython/main.py
@@ -4,6 +4,8 @@ from flask import request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 import geojson
+import random
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://S3ba26:Facebook2015@cluster0.ztaqymo.mongodb.net/pokemon" #Importante qui va specificato il nome del DB
 
@@ -19,6 +21,43 @@ def get_all():
         output.append({"lat": s['lat'], "lng":s['lng']})
     return jsonify(output)
 
+@app.route('/pikachu', methods=['GET'])
+def getpikachu():
+    tane = mongo.db.tane
+    output = []
+    for s in tane.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+
+    return jsonify(output[random.randrange(0, len(output))])
+
+@app.route('/charmender', methods=['GET'])
+def getcharmender():
+    tane = mongo.db.tane
+    output = []
+    for s in tane.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+
+    return jsonify(output[random.randrange(0, len(output))])
+
+@app.route('/bulbasaur', methods=['GET'])
+def get_bulbasaur():
+    tane = mongo.db.tane
+    output = []
+    for s in tane.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+
+    return jsonify(output[random.randrange(0, len(output))])
+
+@app.route('/snorlax', methods=['GET'])
+def get_snorlax():
+    tane = mongo.db.tane
+    output = []
+    for s in tane.find():
+        output.append({"lat": s["lat"], "lng":s["lng"]})
+
+    return jsonify(output[random.randrange(0, len(output))])
+    
+    
 if __name__ == "__main__":
     app.run()
   
